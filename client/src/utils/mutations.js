b6f29c6110a1afc90237cbcf5463fbf47613040c
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-16 21:37:17 -0700

    add @apollo/client graphql queries & mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation LoginUser($email: String!, $password: String!) {
+  loginUser(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+  }
+}
+`;
+
+export const ADD_USER = gql`
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation SaveBook($book: BookUpdate!) {
+  saveBook(book: $book) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      image
+      link
+      title
+    }
+
+  }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation RemoveBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      image
+      link
+      title
+    }
+  }
+}
+`;
